52542381e4f9b9333fbfdb98359ebb4b7576fa78
jwdlplus <jwqueen04@gmail.com>
2025-11-06 16:01:10 +0900

    10 실습6

--- main.c
+++ main.c
@@ -5,18 +5,30 @@ int main(int argc, char *argv[])
 {
   char input[100];
   FILE *fp;
-  int i;
+  char c;
   
-  fp = fopen("sample.txt","w");
-  
-  for(i=0;i<3;i++)
+  fp = fopen("sample.txt","r");
+  if (fp == NULL)
   {
-                  printf("input a word: ");
-                  scanf("%s", input);
-                  fprintf(fp, "%s\n", input);
+         printf("Failed to open file!\n");
+         return -1;
+  }
+  
+  #if 1
+  while( (c = fgetc(fp)) != EOF )
+  {
+         putchar(c);
+  }
+  #endif
+  
+  #if 0
+  while ( fgets(input, 100, fp) >  0)
+  {
+        printf("%s", input);
   }
   
   fclose(fp);
+  #endif
   
   system("PAUSE");	
   return 0;
